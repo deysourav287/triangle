69ebf8f300a1b61bc37d71a051fe5597616ae869
Sourav Dey <106477719+deysourav287@users.noreply.github.com>
2022-07-04 22:09:56 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,9 @@
+a= int(input("enter the length of first side :"))
+b= int(input("enter the length of second side :"))
+area= 0.5*b*a 
+h= (a*a+b*b)**0.5
+p= a+b+h 
+print(" area of triangle:", area)
+print(" length of hypotanuse:", h)
+print(" perimeter:", p)
+		
